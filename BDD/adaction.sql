ac5c6062dbeb3ecd3acd218a607a8eeba120277b
Marinegrn <marinegarein@outlook.fr>
2025-05-27 18:45:06 +0200

    update: review BDD data Dev

--- BDD/adaction.sql
+++ BDD/adaction.sql
@@ -138,7 +138,7 @@ ALTER TABLE adaction_connections
 -- 4. volunteers <-> donations : One-to-Many (un bénévole peut effectuer plusieurs dons)
 -- 5. volunteers <-> wastes : One-to-Many (un bénévole collecte plusieurs déchets via waste_collection)
 -- 6. volunteers <-> points : One-to-Many (un bénévole possède plusieurs points)
--- 7. wastes <-> points : One-to-Many (un déchet peut générer plusieurs points)
+-- 7. wastes <-> points : One-to-Many (un déchet peut générer plusieurs points), à modifier
 -- 8. donations <-> points : One-to-Many (un don équivaut plusieurs points ?)
 -- 9. associations <-> points : One-to-Many (une association recevoir plusieurs points)
 -- 10. adaction_connections : Table de jointure Many-to-Many reliant toutes les entités principales (bénévoles, déchets, villes, associations, dons, points)
@@ -151,3 +151,10 @@ DROP COLUMN "donation";
 -- insertion des valeurs
 -- jointure
 
+-- review Data / BDD : 
+-- conserver la table wastes, mais y rajouter une colonne points (paramètre du déchet, ex: mégot = 10 pts)
+-- table wastes (table de paramétrages) : supprimer colonne quantity, suppr city, et ajouter les points associés
+-- création table collecte : id UUID, volunteer_id, city, date (opt: total de points si gamification)
+-- création table isCollected : collect_id, waste_id, quantity -> all fk ! -> table d'action
+-- Eviter de faire du N:N -> conflit interne en base de données 
+    -- ex: plusieurs maisons peuvent avoir plusieurs fenêtres, donc on ne sait pas quelle fenêtre appartient à telle ou telle maison
