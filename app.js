42fa14e4f3292b87f6bdf77f1dfaddeb199174df
EvaTP <evathpr@icloud.com>
2025-05-27 17:24:14 +0200

    add: routes

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
-const express = require('express')
-var cors = require('cors')
-const app = express()
+const express = require('express');
+var cors = require('cors');
+const app = express();
 
 const volunteers = require('./volunteers.json');
 
@@ -20,8 +20,37 @@ app.get('/volunteers', (req, res) => {
   res.json(volunteers);
 });
 
+// filtrage par nom de famille
+// app.get('/volunteers/:lastname', (req, res)=>{
+//   const filter = req.params.lastname.toLowerCase();
+//       const results = volunteers.filter(volunteer =>
+//       volunteer.lastname.toLowerCase() === filter
+//       );
+//     if (results.length > 0) {
+//       res.json(results);
+//     } else {
+//       res.status(404).json({ error: 'Aucun volontaire trouvé avec ce nom de famille' });
+//     }
+// });
+
+// filtrage par toutes les clés
+app.get('/volunteers', (req, res)=>{
+  let results = volunteers;
+  for (const [key, value] of Object.entries(req.query)){
+    results = results.filter(volunteer =>
+      volunteer[key] && volunteer[key].toString().toLowerCase() === value.toLowerCase()
+    );
+  }
+  if (results.lenght > 0){
+    res.json(results);
+  }else{
+    res.status(404).json({error : 'Aucun volontaire trouvé avec ces critères'});
+  }
+});
+ 
+
 
 
 app.listen(port, () => {
-  console.log(`test ${port}`)
+  console.log(`serveur démarré sur le port  ${port}`)
 })
