983072b3c68d4f983cdd1a47ba4ed45b019a6b7f
EvaTP <evathpr@icloud.com>
2025-05-27 15:43:58 +0200

    add: volunteeers route

--- app.js
+++ app.js
@@ -2,13 +2,25 @@ const express = require('express')
 var cors = require('cors')
 const app = express()
 
+const volunteers = require('./volunteers.json');
+
 app.use(cors())
 
 const port = 3001
 
+app.use(express.json()); // for parsing application/json
+app.use(express.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+
+
+app.get('/volunteers', (req, res) => {
+  res.json(volunteers);
+});
+
+
 
 app.listen(port, () => {
   console.log(`test ${port}`)
