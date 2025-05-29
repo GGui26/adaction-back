d2a0135a44c7a9360810b77e5e870e2f31b6c798
EvaTP <evathpr@icloud.com>
2025-05-29 17:31:55 +0200

    update : app.js with router

---
+++ app.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+
+const volunteersRoutes = require('./routes/volunteers');
+
+app.use(cors());
+app.use(express.json()); // MIDDLEWARE global
+app.use(express.urlencoded({ extended: true })); // pour encoder
+
+
+app.use((req, res, next) => {
+  console.log(`Requête reçue : ${req.method} ${req.url}`);
+  next();
+});
+
+// router
+app.use('/volunteers', volunteersRoutes);
+
+const port = 3001;
+
+// pour lancer le serveur
+app.listen(port, () => {
+  console.log(`serveur démarré sur le port  ${port}`)
+})
+
+// test de route
+app.get('/test', (req, res) => {
+  console.log("Route /test appelée !");
+  res.send("Test OK");
+});
+
+
