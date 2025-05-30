3a76f17262ca330df3a571f54897e76839dc52c9
Marinegrn <marinegarein@outlook.fr>
2025-05-30 12:29:12 +0200

    update: commits update SQL

--- scripts/seeds/update_volunteers.sql
+++ scripts/seeds/update_volunteers.sql
@@ -1,9 +1,9 @@
--- Foreign Key 
 
--- fk table collections
-ALTER TABLE collections
-    ALTER COLUMN "volunteer_id" TYPE UUID USING "volunteer_id"::uuid; -- Remplacer le type de la colonne "volunteer_id" de la table collections pour pouvoir y mettre la clé étrangère
 
-ALTER TABLE collections
-    ADD CONSTRAINT fk_volunteers_collection
-    FOREIGN KEY ("volunteer_id") REFERENCES volunteers("id");
+
+
+-- fk table is_collected
+ALTER TABLE is_collected
+    ADD CONSTRAINT fk_collection_volunteer
+    FOREIGN KEY ("collection_id") REFERENCES collections("id");
+
