86621f201e01ae8a79e2f4776db12efe0a324652
EvaTP <evathpr@icloud.com>
2025-06-07 12:11:14 +0200

    update: volunteers requests

--- scripts/seeds/update_volunteers.sql
+++ scripts/seeds/update_volunteers.sql
@@ -2,3 +2,20 @@
 ALTER TABLE volunteers
 DROP COLUMN "donation";
 DROP COLUMN "donated_points";
+
+-- add volunteers
+INSERT INTO volunteers (firstname, lastname, email, password, location, created_at) VALUES
+    ('Elena', 'Fernandez', 'elena@gmail.com', 'elena54321', 'Paris', NOW()),
+	('John', 'Doe', 'Johny@gmail.com', 'Jdoe54321', 'Lyon', NOW());
+
+
+-- update volunteers
+UPDATE volunteers
+SET firstname = 'Claire'
+WHERE id = 1;
+
+
+-- delete volunteers
+DELETE FROM volunteers
+WHERE id = 1;
+
